1cf326a2d5554888e8786cf4c3ebed2d66cccc48
shah <sjalal0228@gmail.com>
2013-03-25 15:41:22 -0700

    google map updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,21 @@
+// ================== Document Ready Function ============= //
+    $(document).ready(function() {  
+          $('.carousel').carousel({
+           interval: 2000
+        });
+          spotImage();       
 
-
-$(document).ready(function() {  
-      $('.carousel').carousel({
-      interval: 2000
-    })
-      var spotlight = {  
-         // the opacity of the "transparent" images  
-        opacity : 0.2,  
-  
-        /*the variabless bellow are for width and height of the images so we can make 
-        the list item same size */  
-        imgWidth : $('.spotlightWrapper ul li').find('img').width(170),  
-        imgHeight : $('.spotlightWrapper ul li').find('img').height(112)  
-  
-    };  
-  
-    //set the width and height of the list items same as the images  
-    $('.spotlightWrapper ul li').css({ 'width' : spotlight.imgWidth, 'height' : spotlight.imgHeight });  
-  
-    //when mouse over the list item...  
-    $('.spotlightWrapper ul li').hover(function(){  
-  
-        //...find the image inside of it and add active class to it and change opacity to 1 (no transparency)  
-        $(this).find('img').addClass('active').css({ 'opacity' : 1});  
-  
-        //get the other list items and change the opacity of the images inside it to the one we have set in the spotlight array  
-        $(this).siblings('li').find('img').css({'opacity' : spotlight.opacity}) ;  
-  
-        //when mouse leave...  
-    }, function(){  
-  
-        //... find the image inside of the list item it just left and remove the active class  
-        $(this).find('img').removeClass('active');  
-  
-    });  
-  
-    //when mouse leaves the unordered list...  
-    $('.spotlightWrapper ul').bind('mouseleave',function(){  
-        //find the images and change the opacity to 1 (fully visible)  
-        $(this).find('img').css('opacity', 1);  
-    });
-
+//================== Datepicker plugins =================== //
     $("#datepicker").datepicker({
       beforeShowDay : $.datepicker.noWeekends
     });
+
+//================== Form Validation ====================== //
     //when the page load hide the error message...  
     $('.error').hide();
-    //when click the send button...    
-    $(".button").click(function() {  
-        
+    //when click the send button...      
+      $("#submit").click(function() {     
+    // when click the send button...    
     // start form validation  
       $('.error').hide();  
         var apptDate = $("input#datepicker").val();  
@@ -56,10 +23,10 @@ $(document).ready(function() {
         $("label#date_error").show();    
          return false;  
       }
-
+    //...if the field leave empty show the error message and focus on that field.
       $('.error').hide();  
         var Firstname = $("input#Fname").val();  
-        if (Firstname == "") {  // if the field leave empty show the error message and focus on that field...
+        if (Firstname == "") {  
         $("label#first_name_error").show();  
         $("input#Fname").focus();  
          return false;  
@@ -72,6 +39,30 @@ $(document).ready(function() {
         $("input#Lname").focus();  
          return false;  
       }  
+       
+       $('.error').hide();  
+        var Address = $("input#address").val();  
+        if (Address == "") {  
+        $("label#street_address_error").show();  
+        $("input#address").focus();  
+         return false;  
+      }
+
+      $('.error').hide();  
+        var City = $("input#city").val();  
+        if (City == "") {  
+        $("label#city_name_error").show();  
+        $("input#city").focus();  
+         return false;  
+      }
+
+      $('.error').hide();  
+        var Zip = $("input#zip").val();  
+        if (Zip == "") {  
+        $("label#zip_code_error").show();  
+        $("input#zip").focus();  
+         return false;  
+      }
         var email = $("input#email").val();  
         if (email == "") {  
         $("label#email_error").show();  
@@ -82,27 +73,107 @@ $(document).ready(function() {
         if (phone == "") {  
         $("label#phone_error").show();  
         $("input#phone").focus();  
-         return false;  
-      }  
-      
-      // collect all the data from the field and store it in Ajax object and send them to the data base by Ajax post method
-      //  var dataString = 'Appointment Date='+ apptDate + 'First name='+ Firstname +'Last name='+ Lastname + '&email=' + email + '&phone=' + phone;  
-      //    alert (dataString);return false;  
-      // $.ajax({  
-      //   type: "POST",  
-      //   url: "backlift/sample",  
-      //   data: dataString,  
-      //   success: function() {  
-      //     $('#contact_form').html("<div id='message'></div>");  
-      //     $('#message').html("<h2>Contact Form Submitted!</h2>")  
-      //     .append("<p>We will be in touch soon.</p>")  
-      //     .hide()  
-      //     .fadeIn(1500, function() {  
-      //       $('#message').append("<img id='checkmark' src='images/check.png' />");  
-      //     });  
-      //   } //close sucess 
-      // }); //close ajax  
+      }   
+        addInformation();  
+    }); //close click 
+}); //close document ready
+
+// ================== getLocation Function ========================== //     
+// function getLocation() {
+
+//     var mapOptions = {
+//                 center   : new google.maps.LatLng(33.941024, -84.125417),
+//                 zoom: 10,
+//                 mapTypeId: google.maps.MapTypeId.ROADMAP
+//             };
+//             var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+//             var markerOptions = {
+//                 position : new google.maps.LatLng(37.7831, -122.4039),
+//                 map: map
+//             };
+//             var marker = new google.maps.Marker(markerOptions);
+//                 marker.setMap(map);
+
+//             var infoWindowOptions = {
+//                 content  : 'Lawrenceville Family Dentistry!'
+//             };
+
+//             var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+//                 google.maps.event.addListener(marker,'click',function(e){
+              
+//                 infoWindow.open(map, marker); 
+//         });
+//     }; //end getLocation
+
+// ================== spotImage Function ========================== //     
     
-      return false;     
-    });//close click event  
-  }); //close document ready  
+    function spotImage() { 
+      var spotlight = {  
+         // the opacity of the "transparent" images  
+        opacity : 0.2,  
+  
+        /*the variabless bellow are for width and height of the images so we can make 
+        the list item same size */  
+        imgWidth : $('.spotlightWrapper ul li').find('img').width(170),  
+        imgHeight : $('.spotlightWrapper ul li').find('img').height(112)  
+    };  
+        //set the width and height of the list items same as the images  
+        $('.spotlightWrapper ul li').css({ 'width' : spotlight.imgWidth, 'height' : spotlight.imgHeight });  
+        //when mouse over the list item...  
+        $('.spotlightWrapper ul li').hover(function(){  
+            //...find the image inside of it and add active class to it and change opacity to 1 (no transparency)  
+            $(this).find('img').addClass('active').css({ 'opacity' : 1});  
+            //get the other list items and change the opacity of the images inside it to the one we have set in the spotlight array  
+            $(this).siblings('li').find('img').css({'opacity' : spotlight.opacity}) ;  
+            //when mouse leave...  
+        }, function(){  
+            //... find the image inside of the list item it just left and remove the active class  
+            $(this).find('img').removeClass('active');  
+        });  
+        //when mouse leaves the unordered list...  
+        $('.spotlightWrapper ul').bind('mouseleave',function(){  
+            //find the images and change the opacity to 1 (fully visible)  
+            $(this).find('img').css('opacity', 1);  
+        });
+  };
+
+// ================== Post Function (clloect all the field informaiton and post it to the database) ========================== //
+
+    function addInformation() {
+      var dataString =  {
+          appDate   : $("#datepicker").val(),
+          firstname : $("#Fname").val(),
+          lastname  : $("#Lname").val(),
+          street    : $("#address").val(),
+          city      : $("#city").val(),
+          zip       : $("#zip").val(),
+          email     : $("#email").val(),
+          phone     : $("#phone").val()     
+        };  
+
+    $.ajax({  
+        url     : 'backliftapp/request', 
+        type    : 'POST',   
+        dataType: 'json',
+        data    : dataString,
+        success: function(data) {  
+        console.log(data);
+        clearForm();
+        // alert("Your request has been submitted. We will be in touch soon."); 
+        // $("#rigistration").hide();
+        // $('#message').html('<h3>Your request has been submitted.</h3><br/><h4>We will be in touch soon.</h4>');   
+      } //close sucess 
+    }); //close ajax  
+  }; //close addInformation
+           
+// ================== Clear Form ========================== //
+    function clearForm(){
+      $(".patient_inputs").each(function(){
+      $(this).val("");
+      });
+    }; //close clear
+
+
+      //var dataString= 'Appointment Date:'+ apptDate + 'First name:'+ firstname +'Last name:'+ lastname + 'email:' + email + 'phone:' + phone;  
+      //    alert (dataString);return false;  

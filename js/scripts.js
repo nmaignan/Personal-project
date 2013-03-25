27c67cb35167a4ba5e41f1d202c1d2d003742290
shah <sjalal0228@gmail.com>
2013-03-25 15:49:19 -0700

    updated

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@
            interval: 2000
         });
           spotImage();       
-
+  
 //================== Datepicker plugins =================== //
     $("#datepicker").datepicker({
       beforeShowDay : $.datepicker.noWeekends
@@ -79,32 +79,7 @@
 }); //close document ready
 
 // ================== getLocation Function ========================== //     
-// function getLocation() {
-
-//     var mapOptions = {
-//                 center   : new google.maps.LatLng(33.941024, -84.125417),
-//                 zoom: 10,
-//                 mapTypeId: google.maps.MapTypeId.ROADMAP
-//             };
-//             var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-//             var markerOptions = {
-//                 position : new google.maps.LatLng(37.7831, -122.4039),
-//                 map: map
-//             };
-//             var marker = new google.maps.Marker(markerOptions);
-//                 marker.setMap(map);
-
-//             var infoWindowOptions = {
-//                 content  : 'Lawrenceville Family Dentistry!'
-//             };
-
-//             var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
-//                 google.maps.event.addListener(marker,'click',function(e){
-              
-//                 infoWindow.open(map, marker); 
-//         });
-//     }; //end getLocation
+      
 
 // ================== spotImage Function ========================== //     
     
